7dd43875fe78eeb8e810cd23f6bd8e200d416fa3
Auriles El Haddad <auriles@MacBook-Pro-de-Auriles.local>
2022-06-07 23:46:45 +0200

    Rebuilding the Home Page

--- my-app/src/router/_NavLinks.js
+++ my-app/src/router/_NavLinks.js
@@ -6,122 +6,146 @@ import { NavLink } from "react-router-dom";
 class NavLinks extends Component {
     render() {
         return (
-            <>
-            <div class="container">
-                        <div class="row">
-                            <div class="col">
-                            Column
-                            </div>
-                            <div class="col">
-                            Column
-                            </div>
-                            <div class="col">
-                            Column
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-sm-4">
-                            Column
-                            </div>
-                            <div class="col-sm-4">
-                            Column
-                            </div>
-                            <div class="col-sm-4">
-                            Column
-                            </div>
-                        </div>
-                    </div>
+            <div className="navlinks-container">
                 <div className="navlinks">
-
                     <NavLink to="/side-menus-exemple">
                         <span>SideMenusExemple</span>
                         <i></i>
                     </NavLink>
 
+                    <NavLink to="/headers-exemple">
+                        <span>HeadersExemple</span>
+                        <i></i>
+                    </NavLink>
 
+                    <NavLink to="/products-exemple">
+                        <span>ProductsExemple</span>
+                        <i></i>
+                    </NavLink>
 
-
-
-
-
+                    <NavLink to="/product-collections-exemple">
+                        <span>ProductCollectionsExemple</span>
+                        <i></i>
+                    </NavLink>
                 </div>
 
-                            <nav class="hidden lg:uppercase lg:text-gray-500 lg:tracking-wide lg:font-bold lg:text-xs lg:space-x-4 lg:flex">
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/headers-exemple">HeadersExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/products-exemple">ProductsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/product-collections-exemple">ProductCollectionsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/product-cards-exemple">ProductCardsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/checkouts-exemple">CheckoutsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/carts-exemple">CartsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/tables-exemple">TablesExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/steps-exemple">StepsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/radios-group-exemple">RadiosGroupExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/stats-exemple">StatsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/testimonials-exemple">TestimonialsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/popups-exemple">PopupsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/paginations-exemple">PaginationsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/navbars-exemple">NavbarsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/inputs-exemple">InputsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/forms-exemple">FormsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/filters-exemple">FiltersExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/faqs-exemple">FaqsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/cards-exemple">CardsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/breadcrumbs-exemple">BreadcrumbsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/banners-exemple">BannersExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/buttons-exemple">ButtonsExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/buttons-group-exemple">ButtonsGroupExemple</NavLink>
-                                </a>
-                                <a href="" class="block h-16 leading-[4rem] border-b-4 border-transparent hover:text-red-700 hover:border-current">
-                                    <NavLink to="/footers-exemple">FooterExemple</NavLink>
-                                </a>
-                            </nav>
-            </>
+                <div className="navlinks">
+                    <NavLink to="/product-cards-exemple">
+                        <span>ProductCardsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/checkouts-exemple">
+                        <span>CheckoutsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/carts-exemple">
+                        <span>CartsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/tables-exemple">
+                        <span>TablesExemple</span>
+                        <i></i>
+                    </NavLink>
+                </div>
+
+                <div className="navlinks">
+                    <NavLink to="/steps-exemple">
+                        <span>StepsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/radios-group-exemple">
+                        <span>RadiosGroupExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/stats-exemple">
+                        <span>StatsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/testimonials-exemple">
+                        <span>TestimonialsExemple</span>
+                        <i></i>
+                    </NavLink>
+                </div>
+
+                <div className="navlinks">
+                    <NavLink to="/popups-exemple">
+                        <span>PopupsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/paginations-exemple">
+                        <span>PaginationsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/navbars-exemple">
+                        <span>NavbarsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/inputs-exemple">
+                        <span>InputsExemple</span>
+                        <i></i>
+                    </NavLink>
+                </div>
+
+                <div className="navlinks">
+                    <NavLink to="/forms-exemple">
+                        <span>FormsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/filters-exemple">
+                        <span>FiltersExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/faqs-exemple">
+                        <span>FaqsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/cards-exemple">
+                        <span>CardsExemple</span>
+                        <i></i>
+                    </NavLink>
+                </div>
+
+                <div className="navlinks">
+                    <NavLink to="/breadcrumbs-exemple">
+                        <span>BreadcrumbsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/banners-exemple">
+                        <span>BannersExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/buttons-exemple">
+                        <span>ButtonsExemple</span>
+                        <i></i>
+                    </NavLink>
+
+                    <NavLink to="/buttons-group-exemple">
+                        <span>ButtonsGroupExemple</span>
+                        <i></i>
+                    </NavLink>
+                </div>
+
+                <div className="navlinks">
+                    <NavLink to="/footers-exemple">
+                        <span>FooterExemple</span>
+                        <i></i>
+                    </NavLink>
+                </div>
+            </div>
         );
     }
 }
