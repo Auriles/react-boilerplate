d07d65cc8f0130c8ccdf6b3bfe2e8e444639284f
Auriles El Haddad <auriles@MacBook-Pro-de-Auriles.local>
2022-06-12 22:24:36 +0200

    Finishing buttons group page & breadcrumbs page

--- my-app/src/router/_NavLinks.js
+++ my-app/src/router/_NavLinks.js
@@ -306,7 +306,7 @@ class NavLinks extends Component {
 
                             {/* Route 18 */}
                             <NavLink to="/breadcrumbs-exemple">
-                                <a href="" class="flex items-center px-4 py-2 text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700">
+                                <a href="" class="flex items-center px-4 py-2 text-gray-500 rounded-lg hover:bg-blue-100 hover:text-gray-700">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"/>
                                     </svg>
@@ -326,7 +326,7 @@ class NavLinks extends Component {
 
                             {/* Route 20 */}
                             <NavLink to="/buttons-group-exemple">
-                                <a href="" class="flex items-center px-4 py-2 text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700">
+                                <a href="" class="flex items-center px-4 py-2 text-gray-500 rounded-lg hover:bg-blue-100 hover:text-gray-700">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"/>
                                     </svg>
